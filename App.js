3e00643ba110c70f0cf2bb788db20a460ceee932
nikolajno <nikolaj_norborg@hotmail.com>
2020-10-04 13:52:08 +0200

    componentWillMount - firebase config

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBaa1U7cLxIXE2WZMLKplA7vrEcmCWRmI8",
+      authDomain: "marinefuel-95f1a.firebaseapp.com",
+      databaseURL: "https://marinefuel-95f1a.firebaseio.com",
+      projectId: "marinefuel-95f1a",
+      storageBucket: "marinefuel-95f1a.appspot.com",
+      messagingSenderId: "598074706617",
+      appId: "1:598074706617:web:d748c9e73776ea4d306968",
+      measurementId: "G-HP5KBX6H6W"
+    };
+
+
+    // Vi kontrollerer at der ikke allerede er en initialiseret instans af firebase
+    // Så undgår vi fejlen Firebase App named '[DEFAULT]' already exists (app/duplicate-app).
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }
+  render() {
+    return <AppContainer />;
+  }
 }
 
 const styles = StyleSheet.create({
